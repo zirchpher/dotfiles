702b56ae77da2322f119c559fd0efb73fb14bb5d
zirchpher <zirchpher@gmail.com>
2023-05-03 18:59:25 -0500

    nvim config added

---
+++ nvim/init.lua
@@ -0,0 +1,20 @@
+require("remmian.plugins-setup")
+
+-- Core config
+require("remmian.core.options")
+require("remmian.core.keymaps")
+require("remmian.core.colorscheme")
+
+-- Plugins config
+require("remmian.plugins.comment")
+require("remmian.plugins.nvim-tree")
+require("remmian.plugins.lualine")
+require("remmian.plugins.telescope")
+require("remmian.plugins.nvim-cmp")
+require("remmian.plugins.lsp.mason")
+require("remmian.plugins.lsp.lspsaga")
+require("remmian.plugins.lsp.lspconfig")
+require("remmian.plugins.lsp.null-ls")
+require("remmian.plugins.autopairs")
+require("remmian.plugins.gitsigns")
+require("remmian.plugins.treesitter")
